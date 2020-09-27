8f7f900dbbf59fa4f1c97e5cf358be40df2fea65
Abhinav Ramkumar <abhinavramkumar91@gmail.com>
2020-09-28 03:27:43 +0530

    Add Not found page

---
+++ src/screens/PageNotFound/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+  return (
+    <div className="max-w-lg mx-auto my-8">
+      <p className="text-center text-lg text-gray-700 font-semibold">404! Page not found</p>
+    </div>
+  );
+};
+
+export default PageNotFound;
